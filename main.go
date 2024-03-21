76540f98018a014477b7841df65a8e3efeb69040
Elias Jurado <ridimdev@gmail.com>
2024-03-20 20:31:51 -0500

    idem

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	log.Printf("%+v\n", albums)
-	
+
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -27,12 +27,12 @@ type Album struct {
 	Price  float64 `json:"price"`
 }
 
-var albums = [] Album{	
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.90}, 
+var albums = []Album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.90},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan",Price:23.43},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 23.43},
 }
 
-func getAlbums(c *gin.Context)  {
-	c.IndentedJSON(http.StatusOK,albums)
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
